dae03426ecfbb70e0127075d5ff62d86e693f0a1
Quang Tran <nhatquang304@gmail.com>
2023-10-01 00:08:14 -0400

    chore: added validation pipe in task filters

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,13 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from "class-validator";
 import { TaskStatus } from "../tasks.model";
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus, { message: "Invalid status" })
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
   search?: string;
 }
