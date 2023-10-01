436feff95240875489a3e58499af38f29dff9683
Quang Tran <nhatquang304@gmail.com>
2023-09-30 23:42:05 -0400

    feat(CreateTaskDto): added validation pipe

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,9 @@
+import { IsNotEmpty } from "class-validator";
+
 export class CreateTaskDto {
+  @IsNotEmpty({ message: "Title cannot be empty" })
   title: string;
+
+  @IsNotEmpty({ message: "Description cannot be empty" })
   description: string;
 }
